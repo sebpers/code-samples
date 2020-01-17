d53efa494e9e1b068505b291853e7945694401ab
Michele Byman <michelebyman@gmail.com>
2020-01-17 14:12:38 +0100

    plus minus icon

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -16,9 +16,21 @@
         >
           {{ btnText ? "Copy" : "Copied" }}
         </button>
-        <p>text below</p>
-        {{ codeSnippet }}
-        <a :href="codeSnippet.linkToCode" target="_blank">CodeIcon</a>
+
+        <a class="linkToCode" :href="codeSnippet.linkToCode" target="_blank"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            v-b-tooltip.hover
+            title="Link to code"
+            class="mt-3 mr-1"
+          >
+            <path
+              d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1 10.199l-3.64 1.801 3.64 1.796v2.204l-6-2.935v-2.131l6-2.934v2.199zm8 2.866l-6 2.935v-2.204l3.64-1.796-3.64-1.801v-2.199l6 2.935v2.13z"
+            /></svg
+        ></a>
       </div>
     </div>
   </div>
